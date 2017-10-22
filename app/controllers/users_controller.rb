72d0f5f40c15d3e1ec964253ad5facc8f971909c
Peter de Verteuil <peter@500px.com>
2017-10-22 17:17:08 -0400

    users can now create accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end

fcca41fd45cf73423e3ebfaaab55bf61ef7e01cc
Peter de Verteuil <peter@500px.com>
2017-10-22 17:03:04 -0400

    added signup form for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
 
   root 'static_pages#home'
-
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

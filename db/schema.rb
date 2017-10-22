7b618015db2a50f781cbbdefb06bc0881935fb4e
Peter de Verteuil <peter@500px.com>
2017-10-22 15:34:25 -0400

    added index to users email and uniqueness check

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022190908) do
+ActiveRecord::Schema.define(version: 20171022193224) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

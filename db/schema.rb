6925d36635625c15eaab741b39fdb8e21c0d7276
Peter de Verteuil <peter@500px.com>
2017-10-22 15:45:39 -0400

    set up secure password for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022193224) do
+ActiveRecord::Schema.define(version: 20171022193930) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
